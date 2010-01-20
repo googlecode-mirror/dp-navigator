e468398eb5d33445b169fdb732ecfddf69b379e7
benjamin.huynhkimbang <benjamin.huynhkimbang@01e67322-dda6-11de-aa1f-bbd297b2055c>
2010-01-20 10:54:12 +0000

    - Add basic documentation for installation.

--- trunk/web/backend_dev.php
+++ trunk/web/backend_dev.php
@@ -7,6 +7,7 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
   die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
+/* Make sure that the path is correct for your installation else modify it. */
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('backend', 'dev', true);
