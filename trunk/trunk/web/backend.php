36c77506c6bdddffdfd02490f7255267f73b43d9
benjamin.huynhkimbang <benjamin.huynhkimbang@01e67322-dda6-11de-aa1f-bbd297b2055c>
2010-01-20 10:54:12 +0000

    - Add basic documentation for installation.

--- trunk/trunk/web/backend.php
+++ trunk/trunk/web/backend.php
@@ -1,6 +1,6 @@
 <?php
 
-
+/* Make sure that the path is correct for your installation else modify it. */
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('backend', 'prod', false);
