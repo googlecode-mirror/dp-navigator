4fda78afefb4f764ee0d1467ff299ff1106e09cc
benjamin.huynhkimbang <benjamin.huynhkimbang@01e67322-dda6-11de-aa1f-bbd297b2055c>
2010-01-20 11:30:25 +0000

    - Improve pattern presentation with nl2br()

--- trunk/trunk/apps/frontend/modules/dp/templates/homeSuccess.php
+++ trunk/trunk/apps/frontend/modules/dp/templates/homeSuccess.php
@@ -2,7 +2,7 @@
 
 <p>This website allows several users to edit Design Patterns (DP) in a Pattern Language and then to browse them. A Pattern Language is "a structured method of describing good design practices within a field of expertise." (<a href="http://en.wikipedia.org/wiki/Pattern_language">from Wikipedia</a>)</p>
 
-<p id="patternLanguagePresentation"><?php echo sfConfig::get('app_pattern_language_presentation'); ?></p>
+<p id="patternLanguagePresentation"><?php echo nl2br(sfConfig::get('app_pattern_language_presentation')); ?></p>
 
 
 <ul>
