40d951fc8d25433f80f4bcba2d0f42439c4298d8
benjamin.huynhkimbang <benjamin.huynhkimbang@01e67322-dda6-11de-aa1f-bbd297b2055c>
2010-01-25 15:54:59 +0000

    Add "constructor" theme.

--- trunk/trunk/apps/frontend/templates/layout.php
+++ trunk/trunk/apps/frontend/templates/layout.php
@@ -9,24 +9,44 @@
     <?php include_javascripts() ?>
   </head>
   <body>
-    <div id="menu">
-	  <?php echo link_to('Home', 'dp/home');?>
-	  <?php if ($sf_user->isAuthenticated()): ?>
-	    - Browse [<?php echo link_to('By Categories', 'dp/byCategories');?>, <?php echo link_to('Graph', 'dp/graph');?>, <?php echo link_to('In One Page', 'dp/publish');?>]
-		- Edit [<?php echo link_to('Summary', 'dp/index');?>, <?php echo link_to('New', 'dp/new');?>]	    
-	    <!-- <?php echo link_to('Manage Relation Types', 'relationtype/index');?>
-	    - <?php echo link_to('Manage Categories', 'category/index');?>-->
-		<?php if ($sf_user->hasCredential('admins')): ?>
-	      - <a href="<?php echo public_path('backend.php')?>">Go to Admin Panel</a>
-		<?php endif; ?>
-        - <?php echo link_to('Logout', '@sf_guard_signout') ?>
-	  <?php endif; ?>
-	</div>	
+    <div id="wrapheader" class="wrapper">
+	  <div id="header">
+	    <div class="logo">
+	      <h1>
+		    <a href="">DP-Navigator</a>
+		  </h1>
+		  <h2>
+		    A collaborative tool to write and browse Design Patterns
+		  </h2>
+		</div>
+	  </div>
+	</div>
+	<div id="wrapcontent" class="wrapper">
+	  <div id="content">
+	    <div id="menu">
+	      <?php echo link_to('Home', 'dp/home');?>
+	      <?php if ($sf_user->isAuthenticated()): ?>
+  	        - Browse [<?php echo link_to('By Categories', 'dp/byCategories');?>, <?php echo link_to('Graph', 'dp/graph');?>, <?php echo link_to('In One Page', 'dp/publish');?>]
+		    - Edit [<?php echo link_to('Summary', 'dp/index');?>, <?php echo link_to('New', 'dp/new');?>]	    
+	        <!-- <?php echo link_to('Manage Relation Types', 'relationtype/index');?>
+	        - <?php echo link_to('Manage Categories', 'category/index');?>-->
+		    <?php if ($sf_user->hasCredential('admins')): ?>
+	          - <a href="<?php echo public_path('backend.php')?>">Go to Admin Panel</a>
+		    <?php endif; ?>
+            - <?php echo link_to('Logout', '@sf_guard_signout') ?>
+	      <?php endif; ?>
+	    </div>
+	    <div id="container"
+          <?php echo $sf_content ?>
+        </div>
+      </div>
+	</div>
 
-    <?php echo $sf_content ?>
-
+    <div id="wrapfooter" class="wrapper">
 	<div id="footer">
-	  Propulsed by <a href="http://code.google.com/p/dp-navigator/">DpNavigator</a> designed by MOCAH team (LIP6 Lab)
+	  Propulsed by <a href="http://code.google.com/p/dp-navigator/">DpNavigator</a> designed by MOCAH team (LIP6 Lab)<br/>
+	  Theme inspired by <a href="http://code.google.com/p/wp-constructor/">Constructor</a> for Wordpress
+	</div>
 	</div>
   </body>
 </html>
