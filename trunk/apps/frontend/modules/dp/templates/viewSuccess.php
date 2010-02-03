f2b85b966138b2114a321b9f8b614693aecd7049
benjamin.huynhkimbang <benjamin.huynhkimbang@01e67322-dda6-11de-aa1f-bbd297b2055c>
2010-02-03 11:01:04 +0000

    - Interface : improve navigation between edit, edit relations and view.

--- trunk/apps/frontend/modules/dp/templates/viewSuccess.php
+++ trunk/apps/frontend/modules/dp/templates/viewSuccess.php
@@ -6,7 +6,10 @@
 
 <?php include_partial('viewDp', array('dp' => $dp)) ?>
 
+
+<?php //same menu as above ?>
 <div class="dp-menu">
 <?php echo link_to('Edit', 'dp/edit?id='. $dp->getId())?> - 
+<?php echo link_to('Edit relations with other DP', 'dp/editRelations?id='. $dp->getId())?> - 
 <?php echo link_to('In Graph', 'dp/graph?id='.$dp->getId())?>
 </div>
