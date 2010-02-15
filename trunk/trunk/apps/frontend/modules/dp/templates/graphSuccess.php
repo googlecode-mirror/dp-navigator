0ce934fe012997c9ba7c19c4c00bf1dff22ad7d9
benjamin.huynhkimbang <benjamin.huynhkimbang@01e67322-dda6-11de-aa1f-bbd297b2055c>
2010-02-15 14:04:53 +0000

    - Interface bug: correct bad variable in flash for IE.

--- trunk/trunk/apps/frontend/modules/dp/templates/graphSuccess.php
+++ trunk/trunk/apps/frontend/modules/dp/templates/graphSuccess.php
@@ -1,14 +1,18 @@
+<!-- Warning: two inclusions of SWF. 1 for IE and one for Mozilla. -->
+
 <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="100%" height="600">
 <param name="movie" value="<?php echo  public_path($viewerPath, true)?>">
 <param name="quality" value="high">
-<param name="flashVars" value="pagePath=<?php echo  public_path($pagePath, true)?>?>&xmlPath=<?php echo  public_path($xmlPath, true)?><?php echo ($id>0)?'&rootId='. $id:'' ?>"
+<param name="flashVars" value="pagePath=<?php echo public_path($pagePath, true)?>&xmlPath=<?php echo  public_path($xmlPath, true)?><?php echo ($id>0)?'&rootId='. $id:'' ?>"
 
 <embed src="<?php echo  public_path($viewerPath, true)?>"
-	quality="high"
-	pluginspage="http://www.macromedia.com/go/getflashplayer"
-	type="application/x-shockwave-flash"
-	flashVars="pagePath=<?php echo  public_path($pagePath, true)?>&xmlPath=<?php echo  public_path($xmlPath, true)?><?php echo ($id>0)?'&rootId='. $id:''?>"
-	width="100%"
-	height="600">
-	</embed></object>
+quality="high"
+pluginspage="http://www.macromedia.com/go/getflashplayer"
+type="application/x-shockwave-flash"
+flashVars="pagePath=<?php echo  public_path($pagePath, true)?>&xmlPath=<?php echo  public_path($xmlPath, true)?><?php echo ($id>0)?'&rootId='. $id:''?>"
+width="100%"
+height="600">
+</embed>
+
+</object>
 
